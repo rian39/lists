852cba8eca7419707405233406d78d5538789611
BuildTools <a.mackenzie@lancaster.ac.uk>
2017-07-28 14:07:10 +0100

    adding file to be printed

--- render_tufte.r
+++ render_tufte.r
@@ -4,6 +4,6 @@ library(tint)
 library(tufte)
 #render('lists_conversion_experience.rmd',output_format=tufte_handout())
 #render('lists_conversion_experience.rmd',output_format=ioslides_presentation)
-#render('lists_conversion_experience.rmd',output_format=tintPdf())
-render('lists_conversion_experience.rmd',output_format=word_document2())
+render('reconstruction.rmd',output_format=tintPdf())
+render('reconstruction.rmd',output_format=word_document2())
 
