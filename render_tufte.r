3f2e7a4fbcfeb7c7509e81a276105b7091887df6
BuildTools <a.mackenzie@lancaster.ac.uk>
2017-10-02 14:15:32 +0100

    fixing up lost manuscript bits and references

--- render_tufte.r
+++ render_tufte.r
@@ -4,6 +4,6 @@ library(tint)
 library(tufte)
 #render('lists_conversion_experience.rmd',output_format=tufte_handout())
 #render('lists_conversion_experience.rmd',output_format=ioslides_presentation)
-render('reconstruction.rmd',output_format=tintPdf())
+#render('reconstruction.rmd',output_format=tintPdf())
 render('reconstruction.rmd',output_format=word_document2())
 
