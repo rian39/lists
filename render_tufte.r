cc423a4f0400a0c123775d3dc6df299d2610266a
amackenzie <a.mackenzie@lancaster.ac.uk>
2018-02-08 14:24:02 +0000

    rendering gh-pages version of major revision

--- render_tufte.r
+++ render_tufte.r
@@ -1,4 +1,5 @@
 library(rmarkdown)
 library(bookdown)
-library(tufte)
-render('lists_conversion_experience.rmd',output_file='index.html', output_format=html_document2(toc=TRUE, toc_depth = 1, toc_float=FALSE, theme='journal'))
+library(tint)
+render('reconstruction.rmd',output_format=html_document(), 'index.html')
+
